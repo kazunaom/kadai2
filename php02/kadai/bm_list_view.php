15640c542be7cc0369265dc162ecd1f3ef6bb7f7
kazunaomatsumura <joi3cinc3@gmail.com>
2018-06-02 14:59:04 +0900

    "first commit"

---
+++ php02/kadai/bm_list_view.php
@@ -0,0 +1,30 @@
+<?php
+//1.  DB接続します
+try {
+  $pdo = new PDO('mysql:dbname=gs_db01;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DbConnectError:'.$e->getMessage());
+}
+
+//２．データ登録SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_bm_table ORDER BY id DESC");
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if($status==false) {
+    //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+  while( $result = $stmt->fetch(PDO::FETCH_ASSOC)){ 
+    $view .= "<p>". $result["date"]." - ".$result["id"]
+    ." - ".$result["bookname"];
+   
+  }
+
+}
+?>
---
+++ php02/sample/insert.php
@@ -0,0 +1,39 @@
+<?php
+//1. POSTデータ取得
+//$name = filter_input( INPUT_GET, ","name" ); //こういうのもあるよ
+//$email = filter_input( INPUT_POST, "email" ); //こういうのもあるよ
+
+
+
+$bookname = $_POST["bookname"];
+$url = $_POST["url"];
+$coment = $_POST["coment"];
+
+//2. DB接続します
+try {
+  $pdo = new PDO('mysql:dbname=gs_db01;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DbConnectError:'.$e->getMessage());
+}
+
+
+//３．データ登録SQL作成
+$stmt = $pdo->prepare("INSERT INTO gs_bm_table01(id, bookname, url, coment, date)
+VALUES(NULL, :a1, :a2, :a3, sysdate())");
+$stmt->bindValue(':a1', $bookname, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a2', $url, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$stmt->bindValue(':a3', $coment, PDO::PARAM_STR);  //Integer（数値の場合 PDO::PARAM_INT)
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("QueryError:".$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header("Location: index.php");
+  exit;
+
+}
+?>
